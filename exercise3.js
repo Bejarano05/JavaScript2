b7647e37139f59701e2d6412acfa063394213d1c
Juan Antonio Bejarano Basulto <juanantonbejarano7@gmail.com>
2020-02-16 15:58:25 +0100

    Cambio

--- exercise3.js
+++ exercise3.js
@@ -15,11 +15,35 @@ var cadena = {
         {
             cadenaInvertida=cadenaInvertida+this.sinInvertir().charAt(x);
             x--;
-            
         }
         alert(cadenaInvertida);
-        // return ;
+        // return cadenaInvertida;
     },
+    encriptar : function(){
+        
+        let x = 0;
+        var abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+        let a = this.sinInvertir();
+        let fin = "";
+        for(x; x<a.length;x++)
+        {
+            for(let cont = 0; cont<abc.length;cont+=1)
+            {
+                if(abc[cont]==a.charAt(x))
+                {
+                    fin=fin+abc[cont+1];
+                }
+            }
+            alert(fin.charAt(x));
+            if (fin.charAt(x)===undefined)
+            { 
+                fin.charAt(x)=="a";
+            }
+
+        }
+        alert(fin)
+    },
+
     visualizaCadena : function(cad){
         document.write(cad);
     }
