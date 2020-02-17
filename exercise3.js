6907e83fbb44c7125ca8c12f1190f5c46a0f3723
Juan Antonio Bejarano Basulto <juanantonbejarano7@gmail.com>
2020-02-17 21:37:58 +0100

    Update exercise3.js

--- exercise3.js
+++ exercise3.js
@@ -1,13 +1,13 @@
 //var cadena = new Object();
 var cadena = {
     sinInvertir: function () {
+        //Guardo la cadena en una variable nueva
         let cad = document.getElementById("string").value;
-        //document.write("La cadena sin invertir es: " + cad);
         return cad;
     },
     invertida: function () {
-        //document.write(this.sinInvertir());
-        // Unvierto la cadena
+        // Invierto la cadena
+        //Creo una nueva cadena de caracteres con los caracteres de la principal pasando caracter por caracter pero en sentido inverso.
         let cadenaInvertida = "";
         let x = this.sinInvertir().length;
 
@@ -22,22 +22,22 @@ var cadena = {
 
         let x = 0;
         var abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
-        let a = this.sinInvertir().toLowerCase();
+        let a = this.sinInvertir().toLowerCase(); //Paso toda la cadena a minúscula.
         let fin = "";
+        //Recorremos todo este bucle FOR tantas veces como larga sea nuestra cadena
         for (x; x < a.length; x++) {
             for (let cont = 0; cont < abc.length; cont += 1) {
                 if (a.charAt(x) !== "z") {
                     if (abc[cont] == a.charAt(x)) {
-                        fin = fin + abc[cont + 1];
+                        fin = fin + abc[cont + 1]; //Guardamos en una cadena los caracteres de la anterior +1
                     }
                 }
                 else if(cont===abc.length-1){
-                    fin = fin + "a";
+                    fin = fin + "a"; //Si nuestra cadena contiene una "Z" la convertimos en "A"
                 }
             }
-            /* alert(fin.charAt(x)); */
         }
-        alert(fin)
+        alert(fin);
     },
     desencriptar: function () {
         let x = 0;
@@ -55,9 +55,8 @@ var cadena = {
                     fin = fin + "z";
                 }
             }
-            /* alert(fin.charAt(x)); */
         }
-        alert(fin)
+        alert(fin);
     },
 
     visualizaCadena: function (cad) {
