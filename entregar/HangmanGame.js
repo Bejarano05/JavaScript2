efef90d97e897265598b11c932a98166df6b5d81
Juan Antonio Bejarano Basulto <juanantonbejarano7@gmail.com>
2020-02-26 12:12:50 +0100

    a

--- entregar/HangmanGame.js
+++ entregar/HangmanGame.js
@@ -1,10 +1,23 @@
-var game = {
 
-    cont : function(){
-        let num = document.getElementById("numtr");
-        alert(num);
-    }
-
-
+var words = ["dog", "cat", "lion", "tiger", "parrot"]; //Array con las palabras disponibles.
+var word = words[Math.floor(Math.random() * words.length)]; //Elige una palabra del array anterior
+var wordslength = [];
+for (var i = 0; i < word.length; i++) {
+    wordslength[i] = "_";
 }
+function comprobar() {
     
+    let num = document.getElementById("numtr").value;
+    let letter = document.getElementById("letter").value;
+    let remainingLetters = word.length;
+    while (remainingLetters > 0) {
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === letter) {
+                wordslength[j] = letter;
+                remainingLetters--;
+            }
+        }
+        document.getElementById("theword").innerHTML = wordslength;
+        document.getElementById("numtr").value=num;
+    }
+}
