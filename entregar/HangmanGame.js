b56a9804315fe60fdc70f0f1b4577b733e61179d
Juan Antonio Bejarano Basulto <juanantonbejarano7@gmail.com>
2020-03-04 11:56:01 +0100

    a

--- entregar/HangmanGame.js
+++ entregar/HangmanGame.js
@@ -5,6 +5,7 @@ var wordslength = [];
 var attempts = 7;
 let remainingLetters = word.length;
 alert(word);
+// Calculamos la longitud de la palabra y la expresamos con "_" 
 for (var i = 0; i < word.length; i++) {
     wordslength[i] = "_";
 }
@@ -16,8 +17,10 @@ function comprobar() {
         alert("derrota");
         return;
     }
+    
     else{
         for (var j = 0; j < word.length; j++) {
+            // Comprueba si el caracter introducido está en la palabra inicial y la muestra por pantalla
             if (word[j] == letter) {
                 wordslength[j] = letter;
                 remainingLetters--;
@@ -25,6 +28,7 @@ function comprobar() {
                 document.getElementById("theword").innerHTML = wordslength;
             }
         }
+        //Resta intentos si el caracter no se encuentra en la palabra
         if(!found){
             attempts--;   
             document.getElementById("numtr").value = attempts;         
