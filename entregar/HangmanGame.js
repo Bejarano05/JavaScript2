6ccd7f840595f33e8ef1a4046ec894921634b095
Juan Antonio Bejarano Basulto <juanantonbejarano7@gmail.com>
2020-03-07 13:06:23 +0100

    Update HangmanGame.js

--- entregar/HangmanGame.js
+++ entregar/HangmanGame.js
@@ -1,24 +1,24 @@
 
-var words = ["dog", "cat", "lion", "tiger", "parrot","cow","goat","pig","sheep","llama","prawn","jellyfish","lobster","squid","skunk","bear","eagle","butterfly"]; //Array con las palabras disponibles.
+var words = ["dog", "cat", "lion", "tiger", "parrot","cow","goat","pig","sheep","llama",
+            "prawn","jellyfish","lobster","squid","skunk","bear","eagle","butterfly"]; //Array con las palabras disponibles.
 var word = words[Math.floor(Math.random() * words.length)]; //Elige una palabra del array anterior
 var word2 = word;
 var wordslength = [];
 var attempts = 7;
 let remainingLetters = word.length;
-alert(word);
+//alert(word)
 // Calculamos la longitud de la palabra y la expresamos con "_" 
 for (var i = 0; i < word.length; i++) {
     wordslength[i] = "_";
 }
 function comprobar() {
-    let letter = document.getElementById("letter").value;
 
+    let letter = document.getElementById("letter").value;
     var found = false;
     if(attempts<=0){
         alert("derrota, la palabra era: "+ word2);
-        return;
     }
-    
+
     else{
         for (var j = 0; j < word.length; j++) {
             // Comprueba si el caracter introducido está en la palabra inicial y la muestra por pantalla
@@ -37,6 +37,7 @@ function comprobar() {
             document.getElementById("numtr").value = attempts;         
         } 
     }
+    
     if(remainingLetters==0){        
         alert("Victoria");
     }
